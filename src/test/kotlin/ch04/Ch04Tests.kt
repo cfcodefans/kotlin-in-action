93f35c54ac4071710e8491a2abf574c2acafc15f
cfcodefans <cf.chenfan@gmail.com>
2019-09-13 07:54:05 +0800

    ..

--- src/test/kotlin/ch04/Ch04Tests.kt
+++ src/test/kotlin/ch04/Ch04Tests.kt
@@ -31,3 +31,35 @@ class Ch04Tests {
         Button().click()
     }
 }
+
+class Ch04Tests_1 {
+    companion object {
+        val log: Logger = LoggerFactory.getLogger(Ch04Tests_1::class.java)
+
+        interface Clickable {
+            fun click()
+            fun showOff() = log.info("I'm clickable")
+        }
+
+        interface Focusable {
+            fun setFocus(b: Boolean) = log.info("I ${if (b) "got" else "lost"} focus.")
+            fun showOff() = log.info("I'm focusable!")
+        }
+
+        class Button : Clickable, Focusable {
+            override fun click() = log.info("I was clicked")
+            override fun showOff() {
+                super<Clickable>.showOff()
+                super<Focusable>.showOff()
+            }
+        }
+    }
+
+    @Test
+    fun testDiamondInheirtance() {
+        val button = Button()
+        button.showOff()
+        button.setFocus(true)
+        button.click()
+    }
+}
